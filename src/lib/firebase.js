98f760c5ef3fb89b76c53cccb45b6ea30f44528b
Thuandeptrai <tvc4567@gmail.com>
2021-08-11 01:41:33 +0700

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/auth';
 import 'firebase/storage'; 
 import firebase from "firebase/app";
 const config ={
-    apiKey: "AIzaSyCA1UoDBHsKrQoOoknnlZdo4JxhtcyCd68",
-    authDomain: "insta-818e3.firebaseapp.com",
-    projectId: "insta-818e3",
-    storageBucket: "insta-818e3.appspot.com",
-    messagingSenderId: "381717552639",
-    appId: "1:381717552639:web:d52c981bab8b36f61a5bca",
-    measurementId: "G-428E52GRSS"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
  firebase.initializeApp(config);
 const { FieldValue } = firebase.firestore;
